de7201feb10c34691e069b24ea3dea61dc6a2943
pah <pah@ucodev.org>
2016-03-21 08:39:08 +0000

    v0.2a

--- user/index.php
+++ user/index.php
@@ -2,25 +2,28 @@
 
 /* Author: Pedro A. Hortas
  * Email: pah@ucodev.org
- * Date: 18/03/2016
- * License: GPLv3
+ * Date: 21/03/2016
+ * License: APLv3
  */
 
 /*
  * This file is part of uweb.
  *
- * uweb is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * uWeb - uCodev Low Footprint Web Framework (https://github.com/ucodev/uweb)
+ * Copyright (C) 2014-2016  Pedro A. Hortas
  *
- * uweb is distributed in the hope that it will be useful,
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with uweb.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
